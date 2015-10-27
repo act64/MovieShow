294176991d816b3a3953a7668d6145fcbb2c9417
act64 <leiyi1993@foxmail.com>
2015-10-27 22:31:19 +0800

    first commit with empty application but have picasso

---
+++ app/src/main/java/usst/lei/movieshow/AllMoviesActivityFragment.java
@@ -0,0 +1,29 @@
+package usst.lei.movieshow;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class AllMoviesActivityFragment extends Fragment {
+
+    public AllMoviesActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_all_movies, container, false);
+        ImageView iv= (ImageView) v.findViewById(R.id.testImage);
+        Picasso.with(getActivity()).load(R.mipmap.ic_launcher).into(iv);
+        return v;
+    }
+}
